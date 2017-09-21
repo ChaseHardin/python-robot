ed50779d880190a6a5821ad592b56e7ef8467f02
Chase Hardin <chase.hardin@scribbletechnology.com>
2017-09-20 19:06:29 -0500

    run motor b.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-#!/usr/bin/env python3
-# so that script can be run from Brickman
-
 from ev3dev.ev3 import *
+from time import sleep
 
 Sound.beep().wait()
+
+m = LargeMotor('outB')
+m.run_timed(time_sp=3000, speed_sp=-750)
+print("set speed (speed_sp) = " + str(m.speed_sp))
+sleep(1)
+print("actual speed = " + str(m.speed))
+sleep(4)
