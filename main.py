a995098b3cd1e87a1531cc0bb42583af0c847101
Chase Hardin <chase.hardin@scribbletechnology.com>
2017-09-20 19:10:30 -0500

    move robot forward.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from ev3dev.ev3 import *
-from time import sleep
 
 Sound.beep().wait()
 
-m = LargeMotor('outB')
-m.run_timed(time_sp=3000, speed_sp=-750)
-print("set speed (speed_sp) = " + str(m.speed_sp))
-sleep(1)
-print("actual speed = " + str(m.speed))
-sleep(4)
+
+def drive_forward():
+    print('start motors...')
+    a_motor = LargeMotor('outA')
+    b_motor = LargeMotor('outB')
+
+    a_motor.run_timed(time_sp=3000, speed_sp=-750)
+    b_motor.run_timed(time_sp=3000, speed_sp=-750)
+    print('stop robot...')
+
+# m = LargeMotor('outB')
+# m.run_timed(time_sp=3000, speed_sp=-750)
+# print("set speed (speed_sp) = " + str(m.speed_sp))
+# sleep(1)
+# print("actual speed = " + str(m.speed))
+# sleep(4)
