3794ee41476dec306603eeca1f4470afde233093
Chase Hardin <chase.hardin@scribbletechnology.com>
2017-09-23 12:29:37 -0500

    wait

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from src.drive.drive_command import DriveCommand
+from ev3dev.core import LargeMotor
 
-drive_command = DriveCommand()
+a_motor = LargeMotor('outA')
+b_motor = LargeMotor('outB')
 
-drive_command.move(time_sp=5000, speed_sp=-750)
-drive_command.move(time_sp=5000, speed_sp=750)
+
+def move(time_sp, speed_sp):
+    a_motor.run_timed(time_sp=time_sp, speed_sp=speed_sp)
+    b_motor.run_timed(time_sp=time_sp, speed_sp=speed_sp)
+
+
+move(time_sp=5000, speed_sp=-750)
+a_motor.wait_while('running')
+b_motor.wait_while('running')
+
+move(time_sp=5000, speed_sp=750)
