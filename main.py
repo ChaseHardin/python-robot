f928569cad258639e30fb8ca2cc0f70c784ffc7f
Chase Hardin <chase.hardin@scribbletechnology.com>
2017-09-23 12:33:48 -0500

    add wait until

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ def move(time_sp, speed_sp):
     b_motor.run_timed(time_sp=time_sp, speed_sp=speed_sp)
 
 
-Sound.speak('Starting my motors...').wait()
+Sound.speak('Starting my motors...')
+
+a_motor.wait_while('speaking')
+b_motor.wait_while('speaking')
 
 move(time_sp=5000, speed_sp=-750)
 a_motor.wait_while('running')
