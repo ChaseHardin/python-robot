2d078dc02b0c4c54377790ab10615dede7f466bd
Chase Hardin <chase.hardin@scribbletechnology.com>
2017-09-23 12:22:46 -0500

    update

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from time import sleep
-
 from src.drive.drive_command import DriveCommand
 
 drive_command = DriveCommand()
 
-drive_command.forward()
-sleep(2)
-drive_command.backwards()
+drive_command.move(time_sp=5000, speed_sp=-750)
+drive_command.move(time_sp=5000, speed_sp=750)
--- src/drive/drive_command.py
+++ src/drive/drive_command.py
@@ -6,10 +6,6 @@ class DriveCommand:
     a_motor = LargeMotor('outA')
     b_motor = LargeMotor('outB')
 
-    def forward(self):
-        self.a_motor.run_timed(time_sp=6000, speed_sp=-750)
-        self.b_motor.run_timed(time_sp=6000, speed_sp=-750)
-
-    def backwards(self):
-        self.a_motor.run_timed(time_sp=6000, speed_sp=750)
-        self.b_motor.run_timed(time_sp=6000, speed_sp=750)
+    def move(self, time_sp, speed_sp):
+        self.a_motor.run_timed(time_sp=time_sp, speed_sp=speed_sp)
+        self.b_motor.run_timed(time_sp=time_sp, speed_sp=speed_sp)
