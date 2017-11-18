8a7105a039cb890d8c7c292c9cc2a71905b1084b
Chase Hardin <chase.hardin@scribbletechnology.com>
2017-11-18 12:17:19 -0600

    Add sensor logic.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from ev3dev.core import Sound
+from ev3dev.core import Sound, InfraredSensor
 
 Sound.tone(1500, 2000).wait()
+
+ir = InfraredSensor()
+assert ir.connected, "Connect a single infrared sensor to any sensor port"
+
+ir.mode = 'IR-PROX'
+
+count = 0
+
+while count < 100:
+    print("Distance:: ", ir.value())
+    count += 1
