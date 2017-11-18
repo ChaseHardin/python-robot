f798560164b04308024a549599cd3a25dca7a0d3
Chase Hardin <chase.hardin@scribbletechnology.com>
2017-11-18 11:24:36 -0600

    Make sound

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 from ev3dev.core import Sound
 
-from src.drive.drive_command import DriveCommand
-
-drive_command = DriveCommand()
-
-Sound.tone([(3000, 2000, 400), (800, 1800, 2000)]).wait()
-
-drive_command.move(time_sp=5000, speed_sp=-750)
-drive_command.move(time_sp=5000, speed_sp=750)
-
+Sound.tone(1500, 2000).wait()
